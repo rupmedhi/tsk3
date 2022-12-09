f4e7ac26db74934ff3cd27a156687f220a2e781e
rupmedhi <117652883+rupmedhi@users.noreply.github.com>
2022-12-09 09:09:42 +0530

    task3

---
+++ tsk3-dom.js
@@ -0,0 +1,12 @@
+// var t=document.getElementById("header-title");
+// t.style.borderBottom = "black";
+var t=document.getElementById("header-title");
+t.style.borderBottom ="thick solid #111";
+
+var b=document.getElementsByClassName("list-group-item");
+for(var i=0;i<b.length;i++)
+    {
+        b[i].style.color="#0ed30e";
+        b[i].style.fontWeight="bold";
+      
+    }
